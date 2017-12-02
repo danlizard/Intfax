87cff9428762135d7a53fbe80b6240f7e3c043a0
AIshutin <hazmozavr@gmail.com>
2017-12-02 16:15:40 +0300

    Flask POST

--- flask_test.py
+++ flask_test.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify
 from FromIntToTwit import *
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/getTwits/<title>')
+@app.route('/getTwits/<title>', methods=['POST'])
 def getTwitsWEB(title):
     print("Request is received")
-    arr = getTwits(title)
+    arr = getTwits(request.form["title"])
     ans = dict()
     ans["time"] = arr[0]
     ans["forked"] = str(arr[1])
--- getkeywords.py
+++ getkeywords.py
@@ -4,7 +4,7 @@ import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 charact = set('qwertyuiopasdfghjk lzxcvbnmйцукенгшщзхъфывапролджэячсмитьбю1ё23456780')
 def make_header(raw_text):
-    print(raw_text)
+    #print(raw_text)
     raw_text = list(raw_text.lower())
     q = raw_text
     for i in range(len(raw_text)):
@@ -16,7 +16,7 @@ def make_header(raw_text):
     for el in arr:
         if morph.parse(el)[0].tag.POS in good:
             ans.append(el)
-    print(ans)
+    #print(ans)
     return ans
 if __name__ == "__main__":
     s = 'С космодрома Плесецк запущен "Союз" с военным спутником'
