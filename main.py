ab067601e1b21db140caf98be7c208dcfa1e8942
danlizard <30766104+danlizard@users.noreply.github.com>
2017-12-02 11:10:36 +0300

    start

---
+++ main.py
@@ -0,0 +1,5 @@
+import twitter
+api = twitter.Api(consumer_key='UdoIgisGXrJ2mKJZv5AX1DmFk',
+				consumer_secret='rvkpkgv3CSMJCemj5gN3wSlXuytdnmGKAIvMaZfFvfqCFz1rsd',
+				access_token_key='794640882179907586-o4jhXaJMpnwRmPlZPaYq3zFpKxViUTX',
+				access_token_secret='XbdvesO779rjkxtoS3GbSuvYNiLsCWLGKckPfpyvr1lPa')
