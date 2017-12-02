f48af6b85c4bf02e8d9b0c1c2b2108ab2b5f3f46
AIshutin <hazmozavr@gmail.com>
2017-12-02 14:34:35 +0300

    GetKeyWords

--- getkeywords.py
+++ getkeywords.py
@@ -1,10 +1,20 @@
-import pymorthy2
+import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
-
+charact = set('qwertyuiopasdfghjk lzxcvbnmйцукенгшщзхъфывапролджэ€чсмитьбю1Є23456780')
 def make_header(raw_text):
     print(raw_text)
-
-
-if __name___ == "__main__":
+    raw_text = list(raw_text.lower())
+    q = raw_text
+    for i in range(len(raw_text)):
+        if raw_text[i] not in charact:
+            raw_text[i] = ''
+    arr = ("".join(raw_text)).split()
+    good = {"NOUN", "ADJS", "ADJF", "COMP", "VERB", "INFN", "PRTF", "PRTS", "GRND", "NUMR", "NUMB"}
+    ans = []
+    for el in arr:
+        if morph.parse(el)[0].tag.POS in good:
+            ans.append(el)
+    return ans
+if __name__ == "__main__":
     s = '— космодрома ѕлесецк запущен "—оюз" с военным спутником'
-    pass
+    print(" ".join(make_header(s)))
